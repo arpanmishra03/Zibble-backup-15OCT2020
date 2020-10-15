0c27a63cf0985d23ac9b1342d547ef1225e3320d
arpanechoindia <70320733+arpanechoindia@users.noreply.github.com>
2020-10-15 19:40:44 +0530

    API can generate OTP and verify OTP | Checks if new user then make an entry in database. If existing user then no entry made in database

---
+++ Zibble-backup-15OCT2020/src/main/java/com/zibble/dao/UserDaoImpl.java
@@ -0,0 +1,54 @@
+package com.zibble.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.zibble.entity.UserDetailsUD;
+
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	@Override
+	public UserDetailsUD findByMobileNumberUD(String mobileNumberUD) {
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// now retrieve/read from database using username
+		Query<UserDetailsUD> theQuery = currentSession.createQuery("from UserDetailsUD where mobileNumberUD=:mNumber", UserDetailsUD.class);
+		theQuery.setParameter("mNumber", mobileNumberUD);
+	
+		//List<UserDetailsUD> list=theQuery.list();  
+		//int size = list.size();
+		//System.out.println(size); 
+		
+		UserDetailsUD theUser = null;
+		try {
+			theUser = theQuery.list().get(0);
+		} catch (Exception e) {
+			System.out.println("Exception");
+			theUser = null;
+		}
+	
+		return theUser;
+	}
+
+	@Override
+	public void save(UserDetailsUD theUser) {
+		// get current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// create the user ... finally LOL
+		currentSession.saveOrUpdate(theUser);
+	}
+
+}
---
+++ Zibble-backup-15OCT2020/src/main/java/com/zibble/impl/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.zibble.impl.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zibble.dao.UserDao;
+import com.zibble.entity.UserDetailsUD;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	// need to inject user dao
+	@Autowired
+	private UserDao userDao;
+
+
+	@Override
+	@Transactional
+	public void save(String mobileNumberUD) {
+		UserDetailsUD user = new UserDetailsUD();
+		 // assign user details to the user object
+		user.setMobileNumberUD(mobileNumberUD);
+		
+		 // save user in the database
+		userDao.save(user);
+	}
+
+	}
